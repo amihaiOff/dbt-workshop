a71143ce96a9df25dc96406de9007e553731e575
Amihai Offen <amihaio@amihaios-MacBook-Pro.local>
2025-11-28 11:31:18 +0200

    finish ex2

--- models/staging/stg_orders_with_ingestion.sql
+++ models/staging/stg_orders_with_ingestion.sql
@@ -9,7 +9,7 @@ SELECT
     customer_id,
     order_status,
     order_purchase_timestamp::timestamp as order_purchase_timestamp,
-    DATE(order_purchase_timestamp) as order_date,
+    DATE(order_purchase_timestamp::timestamp) as order_date,
     
     -- INGESTION TIME STRATEGY: Track when we received this data
     CURRENT_TIMESTAMP as ingestion_time,
@@ -19,8 +19,8 @@ SELECT
         WHEN order_id IN (
             SELECT order_id FROM {{ source('olist_data', 'olist_orders') }}
             WHERE MOD(ABS(HASHTEXT(order_id)), 10) = 0  -- 10% of orders
-        ) THEN order_purchase_timestamp + INTERVAL '2 days'
-        ELSE order_purchase_timestamp + INTERVAL '1 hour'
+        ) THEN order_purchase_timestamp::timestamp + INTERVAL '2 days'
+        ELSE order_purchase_timestamp::timestamp + INTERVAL '1 hour'
     END as simulated_arrival_time
 
 FROM {{ source('olist_data', 'olist_orders') }}
@@ -30,7 +30,7 @@ WHERE order_status != 'unavailable'
   
   {% if is_incremental() %}
     -- Only process recently arrived data
-    AND order_purchase_timestamp > (
+    AND order_purchase_timestamp::timestamp > (
         SELECT MAX(order_purchase_timestamp) - INTERVAL '1 day'
         FROM {{ this }}
     )
